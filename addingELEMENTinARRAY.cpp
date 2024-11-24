e07e3b0317e1ef887ce1efa9b0572c77ca42ae7e
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-24 01:59:18 -0800

    Add files via upload

---
+++ addingELEMENTinARRAY.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int element[5];
+	element[0]=1;
+	element[1]=2;
+	element[2]=3;
+	element[3]=4;
+	element[4]=5;
+	cout<<"Element at memory location 0 is:"<<element[0]<<endl;
+	cout<<"Element at memory location 1 is: "<<element[1]<<endl;
+	cout<<"Element at memory loction 2 is: "<<element[2]<<endl;
+	cout<<"Element at memory location 3 is: "<<element[3]<<endl;
+	cout<<"Element at memory location 4 s: "<<element[4]<<endl;
+
+	
+	
+	
+}
